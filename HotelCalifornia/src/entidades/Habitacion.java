9ee9db37be107e673f84edbe9ccc216de3e4a52d
santi992 <santisanchez21792@gmail.com>
2023-10-03 11:13:05 -0300

    Clases armadas

--- HotelCalifornia/src/entidades/Habitacion.java
+++ HotelCalifornia/src/entidades/Habitacion.java
@@ -11,4 +11,60 @@ public class Habitacion {
     private boolean reserva;
 
     private boolean estado;
+
+    public Habitacion() {
+    }
+
+    public Habitacion(int idHabitacion, TipoHabitacion tipoHabitacion, int piso, boolean reserva, boolean estado) {
+        this.idHabitacion = idHabitacion;
+        this.tipoHabitacion = tipoHabitacion;
+        this.piso = piso;
+        this.reserva = reserva;
+        this.estado = estado;
+    }
+
+    public int getIdHabitacion() {
+        return idHabitacion;
+    }
+
+    public void setIdHabitacion(int idHabitacion) {
+        this.idHabitacion = idHabitacion;
+    }
+
+    public TipoHabitacion getTipoHabitacion() {
+        return tipoHabitacion;
+    }
+
+    public void setTipoHabitacion(TipoHabitacion tipoHabitacion) {
+        this.tipoHabitacion = tipoHabitacion;
+    }
+
+    public int getPiso() {
+        return piso;
+    }
+
+    public void setPiso(int piso) {
+        this.piso = piso;
+    }
+
+    public boolean isReserva() {
+        return reserva;
+    }
+
+    public void setReserva(boolean reserva) {
+        this.reserva = reserva;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public String toString() {
+        return "Habitacion{" + "idHabitacion=" + idHabitacion + ", tipoHabitacion=" + tipoHabitacion + ", piso=" + piso + '}';
+    }
 }
--- HotelCalifornia/src/entidades/TipoHabitacion.java
+++ HotelCalifornia/src/entidades/TipoHabitacion.java
@@ -11,4 +11,68 @@ public class TipoHabitacion {
     private int cantCamas;
 
     private double precioxNoche;
+
+    public TipoHabitacion() {
+    }
+
+    public TipoHabitacion(int idTipoHab, String nombre, int cantPersonas, int cantCamas, double precioxNoche) {
+        this.idTipoHab = idTipoHab;
+        this.nombre = nombre;
+        this.cantPersonas = cantPersonas;
+        this.cantCamas = cantCamas;
+        this.precioxNoche = precioxNoche;
+    }
+
+    public TipoHabitacion(String nombre, int cantPersonas, int cantCamas, double precioxNoche) {
+        this.nombre = nombre;
+        this.cantPersonas = cantPersonas;
+        this.cantCamas = cantCamas;
+        this.precioxNoche = precioxNoche;
+    }
+
+    public int getIdTipoHab() {
+        return idTipoHab;
+    }
+
+    public void setIdTipoHab(int idTipoHab) {
+        this.idTipoHab = idTipoHab;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getCantPersonas() {
+        return cantPersonas;
+    }
+
+    public void setCantPersonas(int cantPersonas) {
+        this.cantPersonas = cantPersonas;
+    }
+
+    public int getCantCamas() {
+        return cantCamas;
+    }
+
+    public void setCantCamas(int cantCamas) {
+        this.cantCamas = cantCamas;
+    }
+
+    public double getPrecioxNoche() {
+        return precioxNoche;
+    }
+
+    public void setPrecioxNoche(double precioxNoche) {
+        this.precioxNoche = precioxNoche;
+    }
+
+    @Override
+    public String toString() {
+        return "TipoHabitacion{" + "idTipoHab=" + idTipoHab + ", nombre=" + nombre + ", precioxNoche=" + precioxNoche + '}';
+    }
+    
 }
